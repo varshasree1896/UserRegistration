817a9a9564d88ce84d72234d4226c2829da364a5
Pooja Sree <you@example.com>
2022-03-29 21:19:46 +0530

    Added:UC1ValidateFirstName

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,5 +1,14 @@
 package com.userregistration;
 
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+
+public static boolean firstName(String name) {
+        boolean check = Pattern.matches("([A-Z][a-z]{3,})", name);
+        return check;
+
 
 
 }
--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -1,9 +1,16 @@
 package com.userregistration;
 
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class UserRegistrationTest {
-
-
-
+    @Test
+    public void checkValidName() {
+        boolean check = UserRegistration.firstName("Varsha");
+        Assertions.assertSame(true, check);
     }
+}
+
+
+
+
